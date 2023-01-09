da63420b9f4577364b491408917e04e7683eb743
harsimrat <harsimratsingh_1997@hotmail.com>
2023-01-09 21:57:12 +0530

    Chat GPT initial commmit

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,16 +1,31 @@
-//Constants
-const myImage = document.querySelector('img');
-const intro = document.querySelector('p');
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the featured items section
+  var featuredSection = document.querySelector('#featured');
 
+  // Get the category links
+  var categoryLinks = document.querySelectorAll('ul a');
 
-//Image changer
-myImage.onclick = () => {
-    const mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/1.jpg') {
-      myImage.setAttribute('src','images/2.jpg');
-      intro.textContent = "Woah!";
-    } else {
-      myImage.setAttribute('src','images/1.jpg');
-      intro.textContent = "Hello Work";
-    }
-}
+  // Add an event listener to each category link
+  categoryLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      var categoryId = this.getAttribute('href');
+      var category = document.querySelector(categoryId);
+      featuredSection.style.display = 'none';
+      category.style.display = 'flex';
+    });
+  });
+
+  // Add an event listener to the "Featured Items" link
+  var featuredLink = document.querySelector('#featured a');
+  featuredLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    featuredSection.style.display = 'flex';
+    categoryLinks.forEach(function(link) {
+      var categoryId = link.getAttribute('href');
+      var category = document.querySelector(categoryId);
+      category.style.display = 'none';
+    });
+  });
+});
